3e74281c1b3b8a215dfd57d2f9ebde8e32bbdc95
Sharpie1112 <87902616+Sharpie1112@users.noreply.github.com>
2023-12-29 20:41:10 -0500

    implement contact form with web3forn

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -12,4 +14,32 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
+
+    var contactForm = document.querySelector('.contact-form');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var formData = new FormData(this);
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+        if (data.success) {
+            // Reset the form here
+            this.reset();
+            alert('Thank you for reaching! I will be in contact soon');
+        } else {
+            // Handle the error
+            alert('There was an error submitting the form. Please try again.');
+        }
+        })
+        .catch(error => {
+        // Handle the network error
+            alert('Network error: ' + error.message);
+        })
+    });
+
 })();
+
